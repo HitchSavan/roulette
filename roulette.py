b764a5b7a19b34eb735addb07c94ad233cf124e4
Daniil Temnyj <temnyy1@gmail.com>
2025-02-14 11:44:58 +0300

    Add types to functions params

--- roulette.py
+++ roulette.py
@@ -1,17 +1,17 @@
 # needs configuring
-def get_spins_amount(spins_amount_coeff, spin_time):
+def get_spins_amount(spins_amount_coeff: int, spin_time: float):
     spins_amount = spins_amount_coeff * spin_time
     return spins_amount
 
-def get_initial_speed(target_angle, spins_amount, target_time, initial_angle = 0):
+def get_initial_speed(target_angle: float, spins_amount: int, target_time: float, initial_angle: float = 0):
     total_path = target_angle + 360 * spins_amount - initial_angle
     initial_speed = 2 * total_path / target_time
     return initial_speed
 
-def get_acceleration(initial_speed, target_time):
+def get_acceleration(initial_speed: float, target_time: float):
     return -initial_speed / target_time
 
-def get_pos(initial_speed, acceleration, cur_time, initial_angle=0):
+def get_pos(initial_speed: float, acceleration: float, cur_time: float, initial_angle: float = 0):
     cur_speed = initial_speed + acceleration * cur_time
     if cur_speed <= 0:
         cur_speed = 0
@@ -20,6 +20,6 @@ def get_pos(initial_speed, acceleration, cur_time, initial_angle=0):
 
     return position % 360, cur_speed
 
-def angle_to_sectors_amount(angle, sectors_amount):
+def angle_to_sectors_amount(angle: float, sectors_amount: int):
     sector_size = 360 / sectors_amount
     return int((360 - angle) / (sector_size)) % sectors_amount
