7910c79b8b4044e848a5e5e8a42ae15da933a7b5
Daniil Temnyj <temnyy1@gmail.com>
2025-04-02 17:11:02 +0300

    Add template functions

--- roulette.py
+++ roulette.py
@@ -6,6 +6,7 @@ Contains functions for calculating roulette wheel moving.
 We assume that the wheel rotates uniformly with constant 
 negative acceleration.
 """
+from enum import Enum
 
 import utils
 
@@ -13,6 +14,23 @@ SPIN_COEF_MIN = 1
 SPIN_COEF_MAX = 10
 SPIN_COEF_MIN_NORM = 0.5
 SPIN_COEF_MAX_NORM = 1.5
+
+
+LINEAR_SPEED_COEF = 0.3
+# percentage of full spin time; total sum must be not greater than 1
+class Stages(Enum):
+    ACCELERATION_STAGE = 0.3
+    LINEAR_STAGE = ACCELERATION_STAGE + 0.3
+    DECCELERATION_STAGE = 1 - ACCELERATION_STAGE
+
+
+def get_current_stage(cur_time: float, total_time: float) -> int:
+    if cur_time < total_time * Stages.ACCELERATION_STAGE.value:
+        return Stages.ACCELERATION_STAGE
+    if cur_time < total_time * Stages.LINEAR_STAGE.value:
+        return Stages.LINEAR_STAGE
+    if cur_time >= total_time * Stages.LINEAR_STAGE.value:
+        return Stages.DECCELERATION_STAGE
 
 
 # needs configuring
@@ -48,13 +66,27 @@ def get_initial_speed(
     return initial_speed
 
 
-def get_acceleration(initial_speed: float, target_time: float) -> float:
+def get_linear_stage_speed(
+    target_angle: float, spins_amount: int, target_time: float, initial_angle: float = 0
+) -> float:
+    """
+    Clalculates wheel `initial speed` from the given `target
+    angle`, `amount of spins`, `spin time` and `initial angle`
+    """
+
+    total_path = target_angle + 360 * spins_amount - initial_angle
+    linear_speed = LINEAR_SPEED_COEF * 2 * total_path / (target_time * Stages.ACCELERATION_STAGE.value)
+
+    return linear_speed
+
+
+def get_acceleration(initial_speed: float, target_time: float, final_speed: float = 0) -> float:
     """
     Calculates wheel acceleration from the given `initial speed`
     and `target time`
     """
 
-    acceleration = -initial_speed / target_time
+    acceleration = (final_speed - initial_speed) / target_time
 
     return acceleration
 
